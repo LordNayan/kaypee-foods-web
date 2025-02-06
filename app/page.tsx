ad7322119a9f3225f9a8107d8181ed5c6fc85960
Nayan Lakhwani <nayan.lakhwani123@gmail.com>
2025-02-06 09:45:06 +0530

    build: fix return types

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Home from "@/component/home/home";
-// import styles from "./page.module.css";
 import Footer from "@/component/common/footer/footer";
 
 export default function HomeWrapper() {
   return (
     <div>
-      {/* <Home /> */}
       <Footer />
     </div>
   );
